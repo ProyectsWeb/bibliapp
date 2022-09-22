3e55cc0dd2cabf1fc9ec99f696aced7259470161
Jorge Peraza <jorgeph2013@gmail.com>
2022-09-22 02:58:02 -0700

    Corrigiendo la ruta de la imagen

--- js/componentes/Encabezado.js
+++ js/componentes/Encabezado.js
@@ -2,7 +2,7 @@ export function Encabezado(){
   
     return `   
     <div class="encabezado">       
-      <img src="./public/bibliapp.png" alt="" class='img-biblia'>               
+      <img src="./bibliapp.png" alt="" class='img-biblia'>               
       <h2 class="titulo">Sagradas Escrituras</h2>
     </div>   
 `;
