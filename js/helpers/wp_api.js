cb1c78aec862db51b7ae2b3423a63f4088fbdab7
Jorge Peraza <jorgeph2013@gmail.com>
2022-09-23 23:59:08 -0700

    Corrigiendo la ruta al backend

--- js/helpers/wp_api.js
+++ js/helpers/wp_api.js
@@ -1,6 +1,7 @@
 
 /* const API_URL = `http://localhost:4000`; */
-const API_URL = `https://bibliapp-ewx9dkwpm-jorgeph2013-gmailcom.vercel.app`;
+/* const API_URL = `https://bibliapp-ewx9dkwpm-jorgeph2013-gmailcom.vercel.app`; */ //Esta es la primera que se envio
+const API_URL = `https://bibliapp-bkn.vercel.app/`;
 const API_NAME_LIBRO = `namelibro`;
 const API_CAP_LIBRO = `capitulo-libro`;
 const API_VERSO_ALEAT = `verso-aleatorio`;
