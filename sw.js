2cb3f0344f5e35dda1f10684c625433947c15663
Jorge Peraza <jorgeph2013@gmail.com>
2022-10-05 00:05:06 -0700

    Ayudando al sw

--- sw.js
+++ sw.js
@@ -3,8 +3,6 @@ importScripts('js/helpers/sw-utils.js');
 const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1'; 
-     
-
 
 const APP_SHELL = [
    /*  '/', */
