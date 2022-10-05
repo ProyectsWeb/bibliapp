b3f4761740ccbd7033e61893c902ac5b42e8bc77
Jorge Peraza <jorgeph2013@gmail.com>
2022-10-05 00:10:56 -0700

    Probando el sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/helpers/sw-utils.js');
 const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1'; 
-
+ 
 const APP_SHELL = [
    /*  '/', */
     'index.html',
